a00096004fd7ae0c0f999415c27d2bcbc5fcd79a
Akash Bansal <akashbansal@Akashs-MacBook-Air.local>
2025-12-31 17:25:35 +0530

    cors

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:5174',
     'https://civilscoach-frontend.vercel.app',
+    'https://www.civilscoach.com',
+    'https://civilscoach.com',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true,
